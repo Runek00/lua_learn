99fe5ea8251ec999a729f616f8b70d75c3459ec1
Runek00 <runek00@gmail.com>
2024-11-18 00:33:15 +0100

    functions

---
+++ calculatesum.lua
@@ -0,0 +1,7 @@
+function calculateSum(sequence)
+	local sum = 0
+	for i = 1, #sequence do
+		sum = sum + sequence[i]
+	end
+	return sum
+end
---
+++ maximum.lua
@@ -0,0 +1,11 @@
+function maximum (s)
+	local max = s[1]
+	local maxi = 1
+	for i = 2, #s do
+		if s[i] > max then
+			max = s[i]
+			maxi = i
+		end
+	end
+	return max, maxi
+end
